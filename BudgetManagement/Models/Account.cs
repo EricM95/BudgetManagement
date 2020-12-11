c6927b6248e8b0255c1f3af865c6ebcaa16bf025
Eric Man <eric.man95@gmail.com>
2020-12-11 23:22:56 +0000

    Added models with entity classes

---
+++ BudgetManagement/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetManagement.Models
+{
+    public class Account
+    {
+        public int AccountID { get; set; }
+        public DateTime Created { get; set; }
+        public decimal InitialBalance { get; set; }
+    }
+}
