7459c0dfd8c5afe2cb8cd8913acf21d705175b31
Eric Man <eric.man95@gmail.com>
2020-12-11 23:33:18 +0000

    Revert "Revert "Added models with entity classes""
    This reverts commit ebb566d7c03ecd15506c80c9af19b59ab890a866.

---
+++ BudgetManagement/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetManagement.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public char Name { get; set; }
+    }
+}
