ad6626c392aa655c853205b9c05e8c9f79445a91
Eric Man <eric.man95@gmail.com>
2021-01-12 16:27:29 +0000

    Added Identity UI pages

---
+++ BudgetManagement/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BudgetManagement.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BudgetManagement.Areas.Identity.IdentityHostingStartup))]
+namespace BudgetManagement.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- BudgetManagement/Models/Account.cs
+++ BudgetManagement/Models/Account.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.IdentityModel;
 using System.Collections.Generic;
 using System.Linq;
 
